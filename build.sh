35b2b912c6300d594ef230a37aea84eeeff6e78d
parmlabs01 <parmlabsltd@gmail.com>
2026-08-19 09:17:07 +0100

    Update build script to create web project

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:$(pwd)/flutter/bin"
 flutter config --enable-web
 flutter pub get
+flutter create . --platforms web
+flutter pub get
 flutter build web --release
